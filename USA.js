9f246c1b3d2761db41c41241e16122adbf702b6e
lucien <lucien@staritgp.com>
2020-01-20 15:43:15 +0800

    完成美国数据

---
+++ USA.js
@@ -0,0 +1,50 @@
+const superagent = require('superagent');
+
+const USAGAMEHTMLURL = 'https://www.nintendo.com/games/game-guide/?pv=true'; //页面url
+const USAGAMEURL = 'https://u3b6gr4ua3-dsn.algolia.net/1/indexes/*/queries';
+
+
+async function requestUSATarget(limit = 250,page = 1,data = [], url = USAGAMEURL) {
+  const formData = {
+    requests: [{
+      indexName: "noa_aem_game_en_us",
+      params: `query=&hitsPerPage=${limit}&maxValuesPerFacet=30&page=${page}&facets=%5B%22generalFilters%22%2C%22platform%22%2C%22availability%22%2C%22categories%22%2C%22filterShops%22%2C%22virtualConsole%22%2C%22characters%22%2C%22priceRange%22%2C%22esrb%22%2C%22filterPlayers%22%5D&tagFilters=`
+    }]
+  };
+  let total = 0;
+  await superagent.post(url)
+      .set({
+        accept: "application/json",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "zh-CN,zh;q=0.9",
+        "content-type": "application/x-www-form-urlencoded",
+        Host: "u3b6gr4ua3-dsn.algolia.net",
+        Origin: "https://www.nintendo.com",
+        Referer: "https://www.nintendo.com/games/game-guide/?pv=true",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "cross-site"
+      })
+      .query({
+        "x-algolia-agent": "Algolia for vanilla JavaScript (lite) 3.22.1;JS Helper 2.20.1",
+        "x-algolia-application-id": "U3B6GR4UA3",
+        "x-algolia-api-key": "9a20c93440cf63cf1a7008d75f7438bf"
+      })
+      .send(JSON.stringify(formData))
+      .then(res => {
+        const { results } = res.body;
+        const { hits,nbPages } = results[0];
+        page += 1;
+        total = nbPages;
+        data.push(...hits);
+      })
+      .catch(err => console.log(err.response.body));
+  if(total >= page) {
+    await requestUSATarget(250,page,data)
+  }
+  return data
+}
+
+module.exports = {
+  requestUSATarget
+};
+
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const {requestHKTarget} = require('./HK');
 const {requestJPTarget} = require('./JP');
+const {requestUSATarget} = require('./USA');
 
 const express = require('express');
 
@@ -7,14 +8,20 @@ const app = express();
 
 app.listen(3000);
 
+//香港数据请求
 app.get('/hk', async (req, res) => {
   const data = await requestHKTarget();
   res.send(data);
 });
-
+//日本数据
 app.get('/jp', async (req, res) => {
   const data = await requestJPTarget();
   console.log(data.length);
+  res.send(data);
+});
+//美国数据
+app.get('/usa', async (req, res) => {
+  const data = await requestUSATarget();
   res.send(data);
 });
 
